176a5c9e16bd58945c5ecdf8e89489ff9e5abe55
kushhh01 <65150039+kushhh01@users.noreply.github.com>
2021-09-08 23:57:11 +0530

    Add files via upload

---
+++ AMCAT(wipro question).py
@@ -0,0 +1,13 @@
+print("Enter the value of N")
+num = int(input())
+
+if(num>=30 and num<=50):
+    print("D")
+elif(num>=51 and num<=60):
+    print("C")
+elif(num>=61 and num<=80):
+    print("B")
+elif(num>=81 and num<=100):
+    print("A")
+else:
+    print("Invalid!")
